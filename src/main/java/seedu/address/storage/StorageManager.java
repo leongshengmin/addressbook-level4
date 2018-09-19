225c241e4a7bed67806f3f77f8d0c924d2d2038e
Shengmin <leongshengmin@gmail.com>
2018-09-19 12:30:58 +0800

    bug fix travis client

--- src/main/java/seedu/address/storage/StorageManager.java
+++ src/main/java/seedu/address/storage/StorageManager.java
@@ -1,12 +1,12 @@
 package seedu.address.storage;
 
 import com.google.common.eventbus.Subscribe;
+
 import seedu.address.commons.core.ComponentManager;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.model.AddressBookChangedEvent;
 import seedu.address.commons.events.storage.DataSavingExceptionEvent;
 import seedu.address.commons.exceptions.DataConversionException;
-import seedu.address.commons.util.FileUtil;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.UserPrefs;
 
